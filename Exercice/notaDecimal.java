761f8f8b278df72c4c0b05da9fed5672066b9e1e
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 18:20:55 -0300

    notaDecimal exercice added

---
+++ Exercice/notaDecimal.java
@@ -0,0 +1,12 @@
+package Exercice;
+
+public class notaDecimal {
+
+    public static void main(String[] args) {
+        double nota1 = 7.89;
+        double nota2 = 3.45;
+        double media = (nota1 + nota2) /2;
+
+        System.out.println(String.format("O resultado da média é igual à: %.2f", media));
+    }
+}
