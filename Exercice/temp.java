6bd655ac92cee8920de5b23a8b20acf8ef3c0120
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 15:52:41 -0300

    temperature exercice added

---
+++ Exercice/temp.java
@@ -0,0 +1,17 @@
+// Converta uma temperatura em graus Celsius para Fahrenheit.
+package Exercice;
+
+public class temp {
+    public static void main(String[] args) {
+
+    double grauCelsius = 28;
+    double grauFahrenheit = (grauCelsius * 1.8) + 32;
+
+    String mensagem = String.format("A temperatura %.2f em grau Celsius é equivalente a %.2fº grau Fahrenheit.", grauCelsius, grauFahrenheit);
+
+    System.out.println(mensagem);
+
+    int grauFahrenheitInteiro = (int) grauFahrenheit;
+    System.out.println(String.format("a temperatura inteira em grau Fahrenheit é igual a %dº", grauFahrenheitInteiro));
+}
+}
