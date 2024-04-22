3d2c171b983c6d5b76e0c1444349888b1d3d5022
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 18:52:33 -0300

    precoQuantidade exercice added

---
+++ Exercice/precoQuantidade.java
@@ -0,0 +1,11 @@
+package Exercice;
+
+public class precoQuantidade {
+    public static void main(String[] args) {
+    double valor = 5.99;
+    int quantidade = 129;
+    double resultado = valor * quantidade;
+    System.out.println(String.format("O valor da quantidade * valor é igual à: R$%.2f", resultado));
+
+    }
+}
