94e0133fa284373dd3ebc988bcaccbabdb995ef2
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 18:56:39 -0300

    conversion exercice added

---
+++ Exercice/dolarForReais.java
@@ -0,0 +1,10 @@
+package Exercice;
+
+public class dolarForReais {
+    public static void main(String[] args) {
+    double valorEmDolares = 25;
+    double reais = 4.94;
+    double conversao = (valorEmDolares * reais);
+    System.out.println(String.format("A conversao do valor em dolar para real é: R$%.2f", conversao));
+    }
+}
