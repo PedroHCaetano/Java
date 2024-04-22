56a95802e2c622b979f263caefe7e7cf98ba9b92
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 11:51:34 -0300

    Main atualizado

--- PrimeiroCodigo/src/Main.java
+++ PrimeiroCodigo/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {   // Criei um classe publica e ela deve ter o msm nome do arquivo
 
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo! Me chamo Pedro Caetano :)");
+        System.out.println("Ese é o Caetano+");
+        System.out.println("Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
     }
 }
