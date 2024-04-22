8f29d0d5aa3559e0723d19a229d5a45f6d2ce8e3
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 18:35:31 -0300

    casting added

---
+++ Exercice/casting.java
@@ -0,0 +1,10 @@
+package Exercice;
+
+public class casting {
+    public static void main (String[] args) {
+
+    double flutuante = 14.5;
+    int flutuanteInteira = (int) flutuante;
+        System.out.println(String.format("O casting de double para int: %d", flutuanteInteira));
+    }
+}
