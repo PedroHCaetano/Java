7823cef8d6a364f861c8514a4d9dcfb4ac5ef409
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 18:41:52 -0300

    charString added

---
+++ Exercice/charString.java
@@ -0,0 +1,11 @@
+package Exercice;
+
+public class charString {
+    public static void main(String[] args) {
+        char letra = 'A';  // char só usa ''
+        String palavra = " torneira";
+        String mensagem = letra + palavra;
+
+        System.out.println(String.format(mensagem));
+    }
+}
