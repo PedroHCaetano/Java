899f51e78e903771385fbab2c125a8d6690eb668
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 00:27:53 -0300

    Estudos exercice added

---
+++ Exercice/estudos.java
@@ -0,0 +1,8 @@
+package Exercice;
+
+public class estudos {
+    public static void main (String[] args) {
+    System.out.println("IntelliJ IDEA é bom");
+    System.out.println("O trabalho é sobre Matriz");
+}
+}
--- Exercice/perfil.java
+++ Exercice/perfil.java
@@ -1,4 +1,4 @@
-package Exercise;
+package Exercice;
 
 public class perfil {
 
