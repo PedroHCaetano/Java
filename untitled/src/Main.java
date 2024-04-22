e36da660dd99c71c010ad88336ca59141a9ebd7c
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-22 00:07:52 -0300

    Hello World

---
+++ untitled/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {   // Criei um classe publica e ela deve ter o msm nome do arquivo
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo! Me chamo Pedro Caetano :)");
+    }
+}
